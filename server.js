cec3f039957dd47eb5edbfc4025e3b7dd253f2fd
lezernack <luks.ezck@gmail.com>
2024-04-30 12:16:17 -0600

    fixed bu

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const mongodb = require("./db/connect");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
 
+const express = require("express");
+const cors = require("cors");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -13,6 +16,6 @@ app
     res.setHeader("Access-Control-Allow-Origin", "*");
     next();
   })
-  .use("/", require("./routes"));
+  .use("/", require("./routes/connect"));
 
 app.listen(PORT, console.log(`listening on port: ${PORT}`));
